9a87d0c1280fa505bb3318815502f5543d87ef0f
André Polomat <andre.polomat@gearboxsoftware.com>
2023-09-15 17:48:35 -0400

    Dragging

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect, useCallback} from 'react';
 import { createRoot } from 'react-dom/client';
 
 import '../styles/index.scss';
@@ -12,26 +12,66 @@ const PointView = ({x,y}) => {
 	);
 }
 
+export default function useGlobalDOMEvents(props) {
+	const eventsRef = useRef({});
+	const events = eventsRef.current;
+
+	useEffect(() => {
+		for (let [key, func] of Object.entries(props))
+		{
+			events[key] = {
+				currentCallback: func,
+				domCallback: (e) =>
+				{
+					events[key].currentCallback(e);
+				}
+			};
+
+			window.addEventListener(key, events[key].domCallback, false);
+		}
+
+		return () => {
+			for (let [key, event] of Object.entries(events)) {
+				window.removeEventListener(key, event.domCallback, false);
+			}
+		};
+	}, []);
+
+	for (let [key, evt] of Object.entries(events))
+	{
+		evt.currentCallback = props[key];
+	}
+}
+
+
 const SvgView = () => {
 	const [view, setView] = useState({zoom: 2, center: new Vec(50, 50)});
 	const [size, setSize] = useState(new Vec(1200, 800));
 
+	const [dragging, setDragging] = useState(null);
+	//const dragging = draggingRef.current;
+
 	const svgRef = useRef(null);
 
-	const onScroll = (e) => {
+	const eventToPoint = (e) => {
+		const pt = svgRef.current.createSVGPoint();
+		pt.x = e.clientX;
+		pt.y = e.clientY;
+		return new Vec(pt.matrixTransform(svgRef.current.getScreenCTM().inverse()));
+	}
+
+	const handleScroll = (e) => {
+		if(dragging)
+		{
+			return;
+		}
+
 		let {zoom, center} = view;
 
 		const ratio = e.deltaY < 0 ? 1.1 : 0.9;
 		const newZoom = zoom * ratio;
 
-		var pt = svgRef.current.createSVGPoint();
-		function cursorPoint(evt){
-		  pt.x = evt.clientX;
-		  pt.y = evt.clientY;
-		  return pt.matrixTransform(svgRef.current.getScreenCTM().inverse());
-		}
-
-		const mousePos = new Vec(cursorPoint(e));
+		const mousePos = eventToPoint(e);
 
 		center = mousePos.sub(mousePos.sub(center).div(ratio));
 
@@ -41,11 +81,55 @@ const SvgView = () => {
 		});
 	};
 
+	const handleMouseUp = (e) => {
+		if(!dragging)
+		{
+			return;
+		}
+
+		setDragging(null);
+	};
+
+	const handleMouseMove = (e) => {
+		if(!dragging)
+		{
+			return;
+		}
+
+		const currentPoint = new Vec(e.clientX, e.clientY);
+		const center = dragging.startViewPoint.add(dragging.startScreenPoint.sub(currentPoint).div(view.zoom));
+		setView({
+			...view,
+			center
+		});
+	};
+
+	const handleMouseDown = (e) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		setDragging({
+			startViewPoint: view.center.clone(),
+			startScreenPoint: new Vec(e.clientX, e.clientY)
+		});
+	};
+
+	useGlobalDOMEvents({
+		mouseup: handleMouseUp,
+		mousemove: handleMouseMove,
+	});
+
 	const viewBoxSize = size.div(view.zoom);
 	const viewBoxStr = `${view.center.x - viewBoxSize.x / 2} ${view.center.y - viewBoxSize.y / 2} ${viewBoxSize.x} ${viewBoxSize.y}`;
 
 	return (
-		<svg ref={svgRef} className="svg-view" onWheelCapture={onScroll} viewBox={viewBoxStr} height={size.y} width={size.x} >
+		<svg ref={svgRef} 
+			className="svg-view"
+			onMouseDown={handleMouseDown} 
+			onWheelCapture={handleScroll} 
+			viewBox={viewBoxStr} 
+			height={size.y} 
+			width={size.x} >
 			<PointView x={50} y={50}/>
 		</svg> 
 	);
